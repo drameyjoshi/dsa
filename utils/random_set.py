f4ea13b97f9977ce3c5aec8f6b18bed32975ec20
drameyjoshi <dr.amey.joshi@gmail.com>
2023-02-25 22:19:55 +0000

    A utility to generate a list of integers.

---
+++ utils/random_set.py
@@ -0,0 +1,51 @@
+#!/bin/python
+import argparse
+import random
+import sys
+
+from typing import List
+
+def generate(a: int, b: int, n: int) -> List[int]:
+    """
+    Returns n randomly generated integers between a and b, both inclusive.
+
+    Parameters:
+    a: lower limit
+    b: upper limit
+    n: number of integers
+
+    Returns:
+    A list of randomly generated integers.
+    """
+    if a > b:
+        b = a
+
+    if n > b - a + 1:
+        n = b - a + 1
+
+    S = set()
+    while len(S) < n:
+        S.add(random.randint(a, b))
+
+    return list(S)
+
+def main(argv):
+    parser = argparse.ArgumentParser(prog='random_set', 
+        description='Generate a set of randomly chosen integers',
+        epilog='')
+
+    parser.add_argument('-m', '--min')
+    parser.add_argument('-M', '--max')
+    parser.add_argument('-s', '--size')
+
+    args = parser.parse_args()
+    low = int(args.min)
+    high = int(args.max)
+    nelems = int(args.size)
+
+    L = generate(low, high, nelems)
+    print(L)
+
+if __name__ == '__main__':
+    main(sys.argv)
+

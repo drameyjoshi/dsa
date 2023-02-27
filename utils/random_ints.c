6209872de9cb20e075d804d346c6a468770dbb55
drameyjoshi <dr.amey.joshi@gmail.com>
2023-02-27 14:31:06 +0000

    Getting random numbers in a given range.

--- utils/random_ints.c
+++ utils/random_ints.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,28 +8,49 @@ int main(int argc, char *argv[])
     time_t now = time(NULL);
     int seed = (int)(now);
     int n_elems = 0;
-    int max_number = RAND_MAX;
+    int min = 0;
+    int max = RAND_MAX;
     int i;
+    int opt;
+    int args_ok = 0;
+    int range;
 
-    void show_help();
+    void show_help_and_exit();
 
-    if (argc >= 2) {
-        n_elems = atoi(argv[1]);
-        if (argc >= 3) {
-            max_number = atoi(argv[2]);
-        }
-    } else {
-        show_help();
-        exit(EXIT_FAILURE);
+    if (argc < 2) {
+        show_help_and_exit();
     }
+
+    while ((opt = getopt(argc, argv, "n:m:M:")) != -1) {
+        switch(opt) {
+            case 'n': n_elems = atoi(optarg);
+                      args_ok = 1;
+                      break;
+            case 'm': min = atoi(optarg); break;
+            case 'M': max = atoi(optarg); break;
+            default: break;
+        }
+    }
+
+    if (args_ok == 0) {
+        show_help_and_exit();
+    }
+
+    if (max < min) {
+        int temp = max;
+        max = min;
+        min = max;
+    }
+
+    range = max - min;
 
     for (i = 0; i < n_elems; i++) {
         int r = rand_r(&seed);
         int s = r;
 
-        if (max_number != RAND_MAX) {
-            float f = ((1.0 * r)/RAND_MAX) * max_number;
-            s = (int)f;
+        if (max != RAND_MAX) {
+            float f = ((1.0 * r)/RAND_MAX) * range;
+            s = min + (int)f;
         }
         printf("%d ", s);
     }
@@ -37,12 +59,15 @@ int main(int argc, char *argv[])
     exit(EXIT_SUCCESS);
 }
 
-void show_help()
+void show_help_and_exit()
 {
-    printf("random_ints n_numbers [max_number].\n");
+    printf("random_ints -n <number of ints> [-m <min number>] [-M <max number>].\n");
     printf("\n");
-    printf("n_numbers: the number of pseudo-random numbers.\n");
-    printf("max_number: the upper limit of the generated numbers.\n");
+    printf("-n: the number of pseudo-random numbers.\n");
+    printf("-m: the lower limit of the generated numbers. Default is 0.\n");
+    printf("-M: the upper limit of the generated numbers. Default is RAND_MAX\n");
     printf("\n");
+
+    exit(EXIT_FAILURE);
 }
 

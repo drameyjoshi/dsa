50cb53ec666794504928c2de57eae62ffe462c50
drameyjoshi <dr.amey.joshi@gmail.com>
2023-02-27 08:41:59 +0000

    Added a few more test cases.

--- binary_tree/test_tree.c
+++ binary_tree/test_tree.c
@@ -12,19 +12,55 @@ void happy_path_1()
     assert(root->left == NULL);
     assert(root->right == NULL);
 
-    add(9, root);
+    root = add(9, root);
     assert(root->left != NULL);
     assert(root->left->data == 9);
 
-    add(5, root);
-    add(-8, root);
-    add(4, root);
-    add(-4, root);
+    root = add(5, root);
+    root = add(-8, root);
+    root = add(4, root);
+    root = add(-4, root);
 
     assert(root != NULL);
-    printf("%d\n", root->data);
-    print(PREORDER, root);
     assert(6 == len(root));
+
+    destroy(root);
+}
+
+void happy_path_2()
+{
+    struct int_tree *root = create(-9);
+    int entries[] = {17, -1, 15, 4, 9, 7, -4, 20, 1};
+    int i;
+
+    for (i = 0; i < 9; i++) {
+        root = add(entries[i], root);
+    }
+
+    assert(find(4, root) != 0);
+    assert(find(13, root) == 0);
+
+    destroy(root);
+}
+
+void happy_path_3()
+{
+    struct int_tree *root = create(11);
+    int entries[] = {10, -2, 17, 4, 20, 2, 0, 15, 7};
+    int i = 0;
+
+    for (i = 0; i < 9; i++) {
+        root = add(entries[i], root);
+    }
+
+    printf("Printing preorder: ");
+    print(PREORDER, root);
+
+    printf("Printing inorder: ");
+    print(INORDER, root);
+
+    printf("Printing postorder: ");
+    print(POSTORDER, root);
 
     destroy(root);
 }
@@ -32,6 +68,8 @@ void happy_path_1()
 int main()
 {
     happy_path_1();
+    happy_path_2();
+    happy_path_3();
 
     return 0;
 }

5469578384f3208af0038b1767cdea8c49de9d7a
drameyjoshi <dr.amey.joshi@gmail.com>
2023-03-13 13:31:28 +0000

    A very rudimentary beautifier

---
+++ sql-beautifier/src/main/java/res/cs/Beautifier.java
@@ -0,0 +1,40 @@
+package res.cs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.*;
+
+import com.github.vertical_blank.sqlformatter.SqlFormatter;
+import com.github.vertical_blank.sqlformatter.core.FormatConfig;
+
+public class Beautifier {
+
+	public Beautifier() {
+		config = FormatConfig.builder().uppercase(false).maxColumnLength(80).build();
+	}
+	
+	public String beautify(final String original) {
+		return SqlFormatter.format(original, config);
+	}
+	
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			String filename = args[0];
+			Path filePath = Path.of(filename);
+			try {
+				String original = Files.readString(filePath);
+				Beautifier b = new Beautifier();
+				logger.log(Level.INFO, b.beautify(original));
+			} catch (IOException e) {
+				logger.log(Level.WARNING, e.getMessage());
+			}
+			
+		} else {
+			logger.log(Level.SEVERE, "SQL file not given on command line.");
+		}
+	}
+
+	private FormatConfig config;
+	private static Logger logger = Logger.getLogger(Beautifier.class.getName());
+}

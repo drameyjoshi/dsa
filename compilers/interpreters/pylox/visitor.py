b54a81a0bbdaee165ae63963d31ec26fb555efe5
drameyjoshi <dr.amey.joshi@gmail.com>
2023-10-22 22:06:32 +0100

    Added arguments to abstract methods.

--- compilers/interpreters/pylox/visitor.py
+++ compilers/interpreters/pylox/visitor.py
@@ -1,54 +1,68 @@
 from abc import abstractmethod
 
+from expr import (
+    Assign,
+    Binary,
+    Call,
+    Getter,
+    Grouping,
+    Literal,
+    Logical,
+    Setter,
+    Super,
+    This,
+    Unary,
+    Variable)
+
+
 class Visitor:
     def __init__(self):
         pass
 
     @abstractmethod
-    def visit_assign_expr(self):
+    def visit_assign_expr(self, assign: Assign):
         pass
 
     @abstractmethod
-    def visit_binary_expr(self):
+    def visit_binary_expr(self, binary: Binary):
         pass
 
     @abstractmethod
-    def visit_call(self):
+    def visit_call(self, call: Call):
         pass
 
     @abstractmethod
-    def visit_getter(self):
+    def visit_getter(self, getter: Getter):
         pass
 
     @abstractmethod
-    def visit_grouping(self):
+    def visit_grouping(self, grouping: Grouping):
         pass
 
     @abstractmethod
-    def visit_literal(self):
+    def visit_literal(self, literal: Literal):
         pass
 
     @abstractmethod
-    def visit_logical(self):
+    def visit_logical(self, logical: Logical):
         pass
 
     @abstractmethod
-    def visit_setter(self):
+    def visit_setter(self, setter: Setter):
         pass
 
     @abstractmethod
-    def visit_super(self):
+    def visit_super(self, super: Super):
         pass
 
     @abstractmethod
-    def visit_this(self):
+    def visit_this(self, this: This):
         pass
 
     @abstractmethod
-    def visit_unary(self):
+    def visit_unary(self, unary: Unary):
         pass
 
     @abstractmethod
-    def visit_variable(self):
+    def visit_variable(self, variable: Variable):
         pass
-

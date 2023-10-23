8304bab5964347f397a6ae537db9198d73b0a4d7
drameyjoshi <dr.amey.joshi@gmail.com>
2023-10-23 10:02:08 +0100

    Changes required by refactoring of ABCs.

--- compilers/interpreters/pylox/visitor.py
+++ compilers/interpreters/pylox/visitor.py
@@ -1,68 +1,58 @@
 from abc import abstractmethod
+from abc_visitor import ABCVisitor
+from abc_expr import ABCExpr
+from typing import Any
 
-from expr import (
-    Assign,
-    Binary,
-    Call,
-    Getter,
-    Grouping,
-    Literal,
-    Logical,
-    Setter,
-    Super,
-    This,
-    Unary,
-    Variable)
+import expr
 
-
-class Visitor:
+class Visitor(ABCVisitor):
     def __init__(self):
         pass
 
     @abstractmethod
-    def visit_assign_expr(self, assign: Assign):
+    def visit_assign_expr(self, assign: ABCExpr) -> Any:
         pass
 
     @abstractmethod
-    def visit_binary_expr(self, binary: Binary):
+    def visit_binary_expr(self, binary: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_call(self, call: Call):
+    def visit_call(self, call: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_getter(self, getter: Getter):
+    def visit_getter(self, getter: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_grouping(self, grouping: Grouping):
+    def visit_grouping(self, grouping: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_literal(self, literal: Literal):
+    def visit_literal(self, literal: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_logical(self, logical: Logical):
+    def visit_logical(self, logical: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_setter(self, setter: Setter):
+    def visit_setter(self, setter: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_super(self, super: Super):
+    def visit_super(self, super: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_this(self, this: This):
+    def visit_this(self, this: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_unary(self, unary: Unary):
+    def visit_unary(self, unary: ABCExpr):
         pass
 
     @abstractmethod
-    def visit_variable(self, variable: Variable):
+    def visit_variable(self, variable: ABCExpr):
         pass

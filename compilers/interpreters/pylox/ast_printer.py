2f3195e69b8201cb1ea12fd1779c53b61ac7a742
drameyjoshi <dr.amey.joshi@gmail.com>
2023-10-23 10:01:19 +0100

    Changes required by refactoring of ABCs.

--- compilers/interpreters/pylox/ast_printer.py
+++ compilers/interpreters/pylox/ast_printer.py
@@ -1,7 +1,6 @@
 from visitor import Visitor
 from plox_token import Token
-from expr import (Expr, Assign, Binary, Call, Grouping,
-                  Getter, Literal, Logical, Setter, Super, This, Unary, Variable)
+from abc_expr import ABCExpr
 from token_types import TokenType
 
 from typing import Any
@@ -9,7 +8,7 @@ from overrides import override
 
 
 class ASTPrinter(Visitor):
-    def __init__(self, expr: Expr):
+    def __init__(self, expr: ABCExpr):
         self.expr = expr
 
     def print(self):
@@ -19,7 +18,7 @@ class ASTPrinter(Visitor):
     def _transform(self, rv: str, part: Any) -> str:
         if isinstance(part, Token):
             return part._lexeme
-        elif isinstance(part, Expr):
+        elif isinstance(part, ABCExpr):
             pass
         else:
             return part.repr()
@@ -29,7 +28,7 @@ class ASTPrinter(Visitor):
         for p in parts:
             if isinstance(p, Token):
                 rv += p._lexeme
-            elif isinstance(p, Expr):
+            elif isinstance(p, ABCExpr):
                 rv += p.accept(self)
             else:
                 rv += p.repr()
@@ -48,57 +47,61 @@ class ASTPrinter(Visitor):
 
         return rv
 
-    @override
-    def visit_assign_expr(self, assign_expr: Assign) -> str:
-        return self._parenthesize_2('=', assign_expr.name, assign_expr.value)
+    #@abstractmethod
+    #def visit_assign_expr(self, assign: ABCExpr):
+    #    pass
 
     @override
-    def visit_binary_expr(self, binary_expr: Binary) -> str:
-        return self._parenthesize(binary_expr.operator._lexeme, binary_expr.left, binary_expr.right)
+    def visit_assign_expr(self, assign: ABCExpr):
+        return self._parenthesize_2('=', assign.name, assign.value)
 
     @override
-    def visit_call(self, call_expr: Call) -> str:
-        return self._parenthesize('call', call_expr.callee, call_expr.args)
+    def visit_binary_expr(self, binary: ABCExpr) -> str:
+        return self._parenthesize(binary.operator._lexeme, binary.left, binary.right)
 
     @override
-    def visit_getter(self, get_expr: Getter) -> str:
-        return self._parenthesize('.', get_expr.from_object, get_expr.name._lexeme)
+    def visit_call(self, call: ABCExpr) -> str:
+        return self._parenthesize('call', call.callee, call.args)
 
     @override
-    def visit_grouping(self, grouping_expr: Grouping) -> str:
-        return self._parenthesize('group', grouping_expr.expression)
+    def visit_getter(self, getter: ABCExpr) -> str:
+        return self._parenthesize('.', get.from_object, get.name._lexeme)
 
     @override
-    def visit_literal(self, literal_expr: Literal) -> str:
-        if literal_expr.value is None:
+    def visit_grouping(self, grouping: ABCExpr) -> str:
+        return self._parenthesize('group', grouping.expression)
+
+    @override
+    def visit_literal(self, literal: ABCExpr) -> str:
+        if literal.value is None:
             return 'nil'
         else:
-            return str(literal_expr.value)
+            return str(literal.value)
 
     @override
-    def visit_logical(self, logical_expr: Logical) -> str:
-        return self._parenthesize(logical_expr.operator._lexeme, logical_expr.left, logical_expr.right)
+    def visit_logical(self, logical: ABCExpr) -> str:
+        return self._parenthesize(logical.operator._lexeme, logical.left, logical.right)
 
     @override
-    def visit_setter(self, setter_expr: Setter) -> str:
-        return self._parenthesize(setter_expr.to_object, setter_expr.name._lexeme, setter_expr.value)
+    def visit_setter(self, setter: ABCExpr) -> str:
+        return self._parenthesize(setter.to_object, setter.name._lexeme, setter.value)
 
     @override
-    def visit_super(self, super_expr: Super) -> str:
-        return self._parenthesize('super', super_expr.method)
+    def visit_super(self, super: ABCExpr) -> str:
+        return self._parenthesize('super', super.method)
 
     @override
-    def visit_this(self, this_expr: This) -> str:
+    def visit_this(self, this: ABCExpr) -> str:
         return 'this'
 
     @override
-    def visit_unary(self, unary_expr: Unary) -> str:
-        return self._parenthesize(unary_expr.operator._lexeme, unary_expr.right)
+    def visit_unary(self, unary: ABCExpr) -> str:
+        return self._parenthesize(unary.operator._lexeme, unary.right)
 
     @override
-    def visit_variable(self, variable: Variable) -> str:
+    def visit_variable(self, variable: ABCExpr) -> str:
         return self._parenthesize(variable.name)
-
+    
     
     
 def main():
